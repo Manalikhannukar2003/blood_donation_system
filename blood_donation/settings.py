22cb7fb0a1e03447518a9e7e6f4b7b375235fa2f
Manalikhannukar2003 <manalikhannukar@gmail.com>
2025-09-10 11:57:04 +0530

    Configure STATIC_ROOT for deployment

--- blood_donation/settings.py
+++ blood_donation/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'blood_donation.urls'
@@ -122,6 +123,9 @@ import os
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
